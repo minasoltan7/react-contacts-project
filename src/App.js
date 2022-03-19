9824e8947825998a0ac86b54976ec2ce47749d6b
Mina Soultan <minasoltan@windowslive.com>
2022-03-19 22:55:23 +0200

    adding searchbar and showing feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts';
+import Proptypes from "prop-types"
+
 
 class App extends Component {
+  state={
+    contacts:[
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jp"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+     ],
+  }
+// A method to handle removing contact when pressing X button
+  removeContact=(contact)=>{
+   this.setState((currentState)=>{
+     return{
+      contacts:currentState.contacts.filter((c)=>{
+        return c.id !== contact.id
+     })
+    }
+    })
+   
+  }
+
+  
   render() {
     return (
-      <div>
-        Hello World
+      <div> 
+      <ListContacts contacts={this.state.contacts} onRemoveContact={this.removeContact} />
       </div>
     );
   }
+}
+ListContacts.Proptypes={
+  contacts:Proptypes.array.isRequired,
+  onDeleteContact:Proptypes.func.isRequired
 }
 
 export default App;
---
+++ src/ListContacts.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+
+class ListContacts extends Component {
+  state = {
+    query: "",
+  };
+
+  updateQuery = (query) =>
+    this.setState({
+      query: query.trim(),
+    });
+
+  clearQuery = () => this.updateQuery("");
+
+  render() {
+    const { onRemoveContact, contacts } = this.props;
+    const { query } = this.state;
+
+    const showingContacts =
+      query === ""
+        ? contacts
+        : contacts.filter((c) =>
+            c.name.toLowerCase().includes(query.toLowerCase())
+          );
+
+    return (
+      <div className="list-contacts">
+        <div className="list-contacts-top">
+          <input
+            className="search-contacts"
+            type="text"
+            placeholder="Search Contacts"
+            value={query}
+            onChange={(event) => this.updateQuery(event.target.value)}
+          />
+        </div>
+        {contacts.length !== showingContacts.length && (
+          <div className="showing-contacts">
+            <span>
+              Now showing {showingContacts.length} of {contacts.length}
+            </span>
+            <button onClick={this.clearQuery}>Show all</button>
+          </div>
+        )}
+        <ol className="contact-list">
+          {showingContacts.map((contact) => {
+            return (
+              <li key={contact.id} className="contact-list-item">
+                <div
+                  className="contact-avatar"
+                  style={{ backgroundImage: `url(${contact.avatarURL})` }}
+                />
+                <div className="contact-details">
+                  <p>{contact.name}</p>
+                  <p>{contact.handle}</p>
+                </div>
+                <button
+                  className="contact-remove"
+                  onClick={() => onRemoveContact(contact)}
+                >
+                  Remove
+                </button>
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default ListContacts;
